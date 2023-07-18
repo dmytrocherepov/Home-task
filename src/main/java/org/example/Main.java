a4a2213ed2ba20998fe38ff8bc5208ce2c58789f
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-07-18 23:46:15 +0300

    added hello mates!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello mates!");
+        System.out.println("Hello mates!!");
     }
 }
